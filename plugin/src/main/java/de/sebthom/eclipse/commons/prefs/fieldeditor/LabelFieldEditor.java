614805deeb809a34335e4ff8efe7482ef2675e13
sebthom <sebthom@users.noreply.github.com>
2021-10-30 13:47:31 +0200

    add LabelFieldEditor

---
+++ plugin/src/main/java/de/sebthom/eclipse/commons/prefs/fieldeditor/LabelFieldEditor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021 by Sebastian Thomschke and contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package de.sebthom.eclipse.commons.prefs.fieldeditor;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * A pseudo field editor for displaying a label.
+ *
+ * @author Sebastian Thomschke
+ */
+public class LabelFieldEditor extends FieldEditor {
+
+   private GridData labelLayoutData;
+
+   public LabelFieldEditor(final String value, @NonNull final Composite parent) {
+      super("unused", value, parent);
+   }
+
+   @Override
+   protected void adjustForNumColumns(final int numColumns) {
+      labelLayoutData.horizontalSpan = numColumns;
+   }
+
+   @Override
+   protected void doFillIntoGrid(final Composite parent, final int numColumns) {
+      labelLayoutData = new GridData(GridData.FILL, GridData.CENTER, false, false, numColumns, 1);
+      getLabelControl(parent).setLayoutData(labelLayoutData);
+   }
+
+   @Override
+   protected void doLoad() {
+      // ignored
+   }
+
+   @Override
+   protected void doLoadDefault() {
+      // ignored
+   }
+
+   @Override
+   protected void doStore() {
+      // ignored
+   }
+
+   @Override
+   public int getNumberOfControls() {
+      return 1;
+   }
+}
