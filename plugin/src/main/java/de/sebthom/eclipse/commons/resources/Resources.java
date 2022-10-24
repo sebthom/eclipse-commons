51f3cba861f16ceb71b09614d3654afd5d0a9368
sebthom <sebthom@users.noreply.github.com>
2022-10-24 22:48:41 +0200

    extend Resources class

--- plugin/src/main/java/de/sebthom/eclipse/commons/resources/Resources.java
+++ plugin/src/main/java/de/sebthom/eclipse/commons/resources/Resources.java
@@ -7,9 +7,13 @@ package de.sebthom.eclipse.commons.resources;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.annotation.Nullable;
@@ -19,16 +23,12 @@ import org.eclipse.jdt.annotation.Nullable;
  */
 public abstract class Resources {
 
-   /**
-    * Returns the uncached last modified timestamp from the file system via {@link File#lastModified()}.
-    * This value may differ (i.e. be more accurate) than {@link IResource#getModificationStamp()}
-    *
-    * @return 0 if the resource does not exist.
-    */
-   public static long lastModified(final @Nullable IResource res) {
-      if (res == null)
-         return 0;
-      return lastModified(res.getLocation());
+   public static IWorkspace getWorkspace() {
+      return ResourcesPlugin.getWorkspace();
+   }
+
+   public static IWorkspaceRoot getWorkspaceRoot() {
+      return ResourcesPlugin.getWorkspace().getRoot();
    }
 
    /**
@@ -42,7 +42,38 @@ public abstract class Resources {
       return path.toFile().lastModified();
    }
 
+   /**
+    * Returns the uncached last modified timestamp from the file system via {@link File#lastModified()}.
+    * This value may differ (i.e. be more accurate) than {@link IResource#getModificationStamp()}
+    *
+    * @return 0 if the resource does not exist.
+    */
+   public static long lastModified(final @Nullable IResource res) {
+      if (res == null)
+         return 0;
+      return lastModified(res.getLocation());
+   }
+
    public static BufferedReader newBufferedReader(final IFile file) throws CoreException {
       return new BufferedReader(new InputStreamReader(file.getContents(true)));
+   }
+
+   public static File toAbsoluteFile(final IResource file) {
+      final var path = file.getRawLocation();
+      if (path == null)
+         throw new IllegalArgumentException("[file.rawLocation] is null");
+      return path.makeAbsolute().toFile();
+   }
+
+   public static Path toAbsolutePath(final IResource file) {
+      return toAbsoluteFile(file).toPath();
+   }
+
+   public static File toProjectRelativeFile(final IResource file) {
+      return file.getProjectRelativePath().toFile();
+   }
+
+   public static Path toProjectRelativePath(final IResource file) {
+      return file.getProjectRelativePath().toFile().toPath();
    }
 }
