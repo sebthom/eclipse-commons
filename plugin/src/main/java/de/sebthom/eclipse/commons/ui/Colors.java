3b198d8a879d2e6cd8a7f116fd4b44929dc19e96
sebthom <sebthom@users.noreply.github.com>
2022-10-27 12:36:04 +0200

    add Colors#get

--- plugin/src/main/java/de/sebthom/eclipse/commons/ui/Colors.java
+++ plugin/src/main/java/de/sebthom/eclipse/commons/ui/Colors.java
@@ -15,27 +15,34 @@ public abstract class Colors {
 
    private static final Display DISPLAY = UI.getDisplay();
 
-   public static final Color WHITE = DISPLAY.getSystemColor(SWT.COLOR_WHITE);
-   public static final Color BLACK = DISPLAY.getSystemColor(SWT.COLOR_BLACK);
+   public static final Color WHITE = get(SWT.COLOR_WHITE);
+   public static final Color BLACK = get(SWT.COLOR_BLACK);
 
-   public static final Color RED = DISPLAY.getSystemColor(SWT.COLOR_RED);
-   public static final Color DARK_RED = DISPLAY.getSystemColor(SWT.COLOR_DARK_RED);
+   public static final Color RED = get(SWT.COLOR_RED);
+   public static final Color DARK_RED = get(SWT.COLOR_DARK_RED);
 
-   public static final Color GREEN = DISPLAY.getSystemColor(SWT.COLOR_GREEN);
-   public static final Color DARK_GREEN = DISPLAY.getSystemColor(SWT.COLOR_DARK_GREEN);
+   public static final Color GREEN = get(SWT.COLOR_GREEN);
+   public static final Color DARK_GREEN = get(SWT.COLOR_DARK_GREEN);
 
-   public static final Color YELLOW = DISPLAY.getSystemColor(SWT.COLOR_YELLOW);
-   public static final Color DARK_YELLOW = DISPLAY.getSystemColor(SWT.COLOR_DARK_YELLOW);
+   public static final Color YELLOW = get(SWT.COLOR_YELLOW);
+   public static final Color DARK_YELLOW = get(SWT.COLOR_DARK_YELLOW);
 
-   public static final Color BLUE = DISPLAY.getSystemColor(SWT.COLOR_BLUE);
-   public static final Color DARK_BLUE = DISPLAY.getSystemColor(SWT.COLOR_DARK_BLUE);
+   public static final Color BLUE = get(SWT.COLOR_BLUE);
+   public static final Color DARK_BLUE = get(SWT.COLOR_DARK_BLUE);
 
-   public static final Color MAGENTA = DISPLAY.getSystemColor(SWT.COLOR_MAGENTA);
-   public static final Color DARK_MAGENTA = DISPLAY.getSystemColor(SWT.COLOR_DARK_MAGENTA);
+   public static final Color MAGENTA = get(SWT.COLOR_MAGENTA);
+   public static final Color DARK_MAGENTA = get(SWT.COLOR_DARK_MAGENTA);
 
-   public static final Color CYAN = DISPLAY.getSystemColor(SWT.COLOR_CYAN);
-   public static final Color DARK_CYAN = DISPLAY.getSystemColor(SWT.COLOR_DARK_CYAN);
+   public static final Color CYAN = get(SWT.COLOR_CYAN);
+   public static final Color DARK_CYAN = get(SWT.COLOR_DARK_CYAN);
 
-   public static final Color GRAY = DISPLAY.getSystemColor(SWT.COLOR_GRAY);
-   public static final Color DARK_GRAY = DISPLAY.getSystemColor(SWT.COLOR_DARK_GRAY);
+   public static final Color GRAY = get(SWT.COLOR_GRAY);
+   public static final Color DARK_GRAY = get(SWT.COLOR_DARK_GRAY);
+
+   /**
+    * @param colorId see color constants in {@link SWT}
+    */
+   public static Color get(final int colorId) {
+      return DISPLAY.getSystemColor(colorId);
+   }
 }
