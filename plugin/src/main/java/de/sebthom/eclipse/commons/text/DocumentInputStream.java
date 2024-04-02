f83be63f390cbf3d2587af3a0190732b3e7cef13
sebthom <sebthom@users.noreply.github.com>
2024-04-02 11:40:24 +0200

    remove unnecessary annotation

--- plugin/src/main/java/de/sebthom/eclipse/commons/text/DocumentInputStream.java
+++ plugin/src/main/java/de/sebthom/eclipse/commons/text/DocumentInputStream.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 
@@ -45,7 +44,7 @@ public class DocumentInputStream extends InputStream {
    }
 
    @Override
-   public int read(@NonNullByDefault({}) final byte[] buff, final int buffOffset, final int len) throws IOException {
+   public int read(final byte[] buff, final int buffOffset, final int len) throws IOException {
       Objects.checkFromIndexSize(buffOffset, len, buff.length);
 
       if (len == 0)
